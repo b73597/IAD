20f1a5e8fc56b472c03fde178e479c3b0bc232bb
Ed <EdDavila@fullsail.edu>
2015-08-27 16:47:44 -0500

    Update HighScoreTableViewController.h
    Merging Computers Work to master branch

--- E_Davila_IAD_LeaderBoards&SocialNetworkShare/FutBoy/MissileMan V.S MuscleMan/HighScoreTableViewController.h
+++ E_Davila_IAD_LeaderBoards&SocialNetworkShare/FutBoy/MissileMan V.S MuscleMan/HighScoreTableViewController.h
@@ -2,7 +2,7 @@
 //  HighScoreTableViewController.h
 //  FutBoy
 //
-//  Created by Hoa Nguyen Van on 8/19/15.
+//  Created by Omar Davila on 8/19/15.
 //  Copyright (c) 2015 Omar Davila. All rights reserved.
 //
 
